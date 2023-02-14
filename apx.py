0c54391ceb034a04ad288045499db413e414b33b
Sam <81489249+samuele-bortolato@users.noreply.github.com>
2023-02-14 17:13:58 +0100

    Initial commit

---
+++ apx.py
@@ -0,0 +1,281 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import os
+import math
+import matplotlib.pyplot as plt
+import time
+
+class ApxSVD(torch.nn.Module):
+    def __init__(self, 
+                embeddings,
+                n):
+        super().__init__()
+        if isinstance(embeddings,torch.nn.parameter.Parameter):
+            embeddings=embeddings.detach().cpu()
+        S,V,D = np.linalg.svd(embeddings,full_matrices=False)
+        self.SV = nn.Parameter(torch.tensor(S[:,:n]*V[:n], dtype=torch.float32))
+        self.D = nn.Parameter(torch.tensor(D[:n], dtype=torch.float32))
+
+    def forward(self, idx):
+        out=self.SV[idx]@self.D
+        return out
+
+
+class Indexing(torch.autograd.Function):
+
+    @staticmethod
+    @torch.cuda.amp.custom_fwd(cast_inputs=torch.half)
+    def forward(ctx, embeds, scores):
+        ctx.save_for_backward(embeds, scores)
+        # embeds.shape                                              # (L, W, C)
+        s = scores.shape                                            # (L, B, W)
+        idx = scores.max(-1)[1].T                                   # (B, L)
+        idx = idx.reshape(-1)                                       # (B*L)
+        out = embeds[list((torch.arange(s[0]).repeat(s[1]), idx))]  # (B*L, C)
+        out = out.reshape(s[1],-1)                                  # (B, L*C)
+        return out
+
+    @staticmethod
+    @torch.cuda.amp.custom_bwd
+    def backward(ctx, grad_output):
+        embeds, scores = ctx.saved_tensors
+        grad_output=grad_output.view(-1, embeds.shape[0], embeds.shape[2])
+
+        grad_embeds = grad_scores = None
+
+        if ctx.needs_input_grad[0]:
+            grad_embeds = torch.bmm(scores.permute(0,2,1), grad_output.permute(1,0,2))
+
+        if ctx.needs_input_grad[1]:
+            grad_scores = torch.bmm(grad_output.permute(1,0,2), embeds.permute(0,2,1))
+
+        return grad_embeds, grad_scores
+
+
+class CodeBook(torch.nn.Module):
+
+    def __init__(self, 
+        levels, 
+        feature_dim,
+        num_words,
+        feature_std         : float        = 1.0,
+        feature_bias        : float        = 0.0,
+        codebook_bitwidth=8):
+
+        super().__init__()
+
+        self.levels=levels
+        self.feature_dim=feature_dim
+        self.num_words=num_words
+        self.feature_std=feature_std
+        self.feature_bias=feature_bias
+        self.bitwidth=codebook_bitwidth
+
+        self.dictionary_size = 2 ** self.bitwidth
+        self.dictionary = nn.Parameter(torch.randn(self.levels, self.dictionary_size, self.feature_dim) * self.feature_std + self.feature_bias)
+        self.feats=nn.Parameter(torch.randn(self.levels, self.num_words, self.dictionary_size))
+        self.indexing_func=Indexing.apply
+    
+    def forward(self, idx):
+        if self.feats is not None:                                                  # logits.shape (L,B,W)
+            logits=self.feats[:,idx.long()] 
+            return self.indexing_func(self.dictionary, logits)
+        else:
+            indices = self.fixed_indices[idx.long()]                             # indices.shape (B,L)
+            indices = indices.reshape(-1).long()                                                 # (B*L)
+            out = self.dictionary[list((torch.arange(self.levels).repeat(len(idx)), indices))]   # (B*L,C)
+            out = out.reshape(len(idx),-1)                                                       # (B, L*C)
+            return out
+    
+    def fix_indices(self):
+        self.fixed_indices = nn.Parameter((self.feats.max(-1)[1]).to(torch.int8).T, requires_grad=False)
+        self.feats = None
+
+
+class ApproxEmbed(torch.nn.Module):
+    def __init__(self, 
+        levels, 
+        feature_dim,
+        num_words,
+        output_dims,
+        feature_std         : float        = 1.0,
+        feature_bias        : float        = 0.0,
+        codebook_bitwidth=8,
+        neurons=64,
+        nn_levels=2):
+        
+        super().__init__()
+   
+        self.B=CodeBook(levels, 
+                        feature_dim,
+                        num_words,
+                        feature_std= feature_std,
+                        feature_bias= feature_bias,
+                        codebook_bitwidth=8)
+
+        # REQUIRES TORCH 2.0 NIGHTLY ON LINUX
+        #self.B=torch.compile(self.B,
+        #            mode='max-autotune',
+        #            fullgraph=True)
+        
+        self.N=torch.nn.Sequential()
+
+        prec_dim = feature_dim*levels
+        for i in range(nn_levels):
+            self.N.append(torch.nn.Linear(prec_dim, neurons))
+            self.N.append(torch.nn.LeakyReLU())
+            prec_dim = neurons
+        self.N.append(torch.nn.Linear(prec_dim, output_dims))
+
+    def forward(self, idx):
+        x = self.B(idx.reshape(-1))
+        x = self.N(x).reshape(*idx.shape,-1)
+        return x
+
+    def fix_indices(self):
+        self.B.fix_indices()
+
+
+
+
+def train_apx(apx, embeddings, epochs=1000, batch_size=2**10, checkpoint_every=100, save_path='', optimizer=None, verbose=False, device='cuda'):
+    
+    if optimizer is None:
+        optimizer=torch.optim.Adam(apx.parameters(),lr=0.01)
+
+    h=[]
+    run={}
+    run['level']=apx.B.levels
+    run['channel']=apx.B.feature_dim
+    best_loss=np.inf
+    
+    order=np.arange(embeddings.shape[0])
+    for epoch in range(epochs):
+        t0=time.time()
+        tot_loss=0
+        np.random.shuffle(order)
+        for batch in range(int(np.ceil(len(order)/batch_size))):
+
+            positions = torch.tensor(order[batch*batch_size:(batch+1)*batch_size],dtype=int,device=device)
+            embed_preds = apx(positions)
+
+            embeds = embeddings[positions]
+            loss=torch.mean(torch.square(embed_preds-embeds))
+
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+            tot_loss+=loss.detach().cpu()
+
+        average_loss=tot_loss.numpy()/int(np.ceil(len(order)/batch_size))
+        h.append(average_loss)
+        if verbose:
+            print(epoch, f'{average_loss:.5g}'.ljust(80), time.time()-t0, end='\r')
+    
+        if epoch%checkpoint_every==checkpoint_every-1:
+            # save the results to be analized later
+            run['loss']=h
+            
+            if tot_loss<best_loss:# save the best model
+                best_loss=tot_loss
+                run['apx']=apx.state_dict()
+                torch.save(run, save_path + str(run['level']) + '_' + str(run['channel'])+'.pth')
+    if verbose:
+        print()
+    apx.load_state_dict(torch.load(save_path + str(run['level']) + '_' + str(run['channel'])+'.pth')['apx'])
+    run['apx']=apx
+    return run
+
+def calc_size(x):
+   if isinstance(x,list) or isinstance(x,torch.nn.ParameterList):
+      tot=np.sum([calc_size(elem) for elem in x])
+   else:
+      if isinstance(x,torch.Tensor):
+         x=x.detach().cpu()
+      x=np.array(x)
+      tot=x.size * x.itemsize
+   return tot
+
+
+def load_runs(runs_folder):
+
+    runs = []
+    for name in os.listdir(runs_folder):
+        run = load_run(runs_folder + name)
+        runs.append(run)
+    return runs
+
+def load_run(filename):
+
+    run=torch.load(filename)
+
+    levels = run['level']
+    channels = run['channel']
+    nn_weights = [run['apx'][p] for p in run['apx'].keys() if '.weight' in p]
+    nn_levels = len(nn_weights)-1
+    neurons=len(nn_weights[0])
+    embeddings_size=len(nn_weights[-1])
+    feats=run['apx']['B.feats']
+    if feats is not None:
+        n_embeddings=feats.shape[1]
+    else:
+        n_embeddings=feats.shape[1]
+    bits = int(math.log2(run['apx']['B.dictionary'].shape[1]))
+
+    apx = ApproxEmbed(levels = levels, 
+                feature_dim = channels,
+                num_words = n_embeddings,
+                output_dims = embeddings_size,
+                feature_std = 0.1,
+                feature_bias = 0.0,
+                codebook_bitwidth=bits,
+                neurons = neurons,
+                nn_levels = nn_levels)
+
+    if feats is None:
+        apx.fix_indices()
+    
+    run['apx']=apx
+    return run
+
+def plot_compare_result_runs(runs, embeddings, bits=8):
+
+    levs={}
+    for run in runs:
+        if run['level'] not in levs.keys():
+            levs[run['level']]=[]
+        levs[run['level']].append(run)
+
+    LEVELS = list(levs.keys())
+    max_s=torch.max(embeddings).item()
+
+    color = plt.cm.viridis(1-np.linspace(0, 1, len(LEVELS)))
+
+    fig, ax = plt.subplots(figsize=(15,10))
+    for l in levs.keys():
+        print(l)
+        size= np.array([calc_size([run['apx'].B.dictionary, list(run['apx'].N.parameters())])+ run['level']*embeddings.shape[0]*bits/8 for run in levs[l]])/1e6
+        loss = np.array([np.min(run['loss']) for run in levs[l]])
+        idx = np.argsort(size)
+        snr = 10*np.log10(max_s/loss)
+        txt = [run['channel'] for run in levs[l]]
+        ax.plot(size[idx], snr[idx], '.', color=color[np.where(np.array(LEVELS)==l)])
+        ax.plot(size[idx], snr[idx], color=color[np.where(np.array(LEVELS)==l)])
+        
+
+    S,V,D = np.linalg.svd(embeddings.detach().cpu(),full_matrices=False)
+
+    # plot 
+    N=list(np.arange(40)+1)
+
+    size=np.array([calc_size([S[:,:n],V[:n],D[:n]]) for n in N])/1e6
+    loss=np.array([torch.mean(torch.square(torch.tensor((S[:,:n]*V[:n])@D[:n],device=embeddings.device)-embeddings)).item() for n in N])
+    snr = 10*np.log10(max_s/loss)
+
+    ax.plot(size, snr,'.', color='r')
+    ax.plot(size, snr, color='r')
+
+    plt.xlabel('Size MB')
+    plt.ylabel('PSNR')
---
+++ glue_score.py
@@ -0,0 +1,205 @@
+import torch
+from torch.utils.data import DataLoader
+import numpy as np
+from transformers import AutoModelForSequenceClassification
+import torchmetrics
+import datasets
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+from tqdm import tqdm
+
+GLUE_TASKS={
+    'cola':{'type':'classification', 'metrics':['Matthews']},
+    'sst2':{'type':'classification', 'metrics':['Accuracy']},
+    'mrpc':{'type':'classification', 'metrics':['F1','Accuracy']},
+    'stsb':{'type':'regression', 'final_activation':'sigmoid', 'max':5, 'metrics':['Pearson','Spearman']},
+    'qqp':{'type':'classification', 'metrics':['F1']},
+    'mnli':{'type':'classification', 'num_classes':3, 'val_datasets':['mnli_mismatched','mnli_matched'], 'metrics':['Accuracy']},
+    'qnli':{'type':'classification', 'metrics':['Accuracy']},
+    'rte':{'type':'classification', 'metrics':['Accuracy']},
+    'wnli':{'type':'classification', 'metrics':['Accuracy']}
+}
+
+def make_model(model_name, args, device='cuda'):
+    if args['type']=='classification':
+        if 'num_classes' in args.keys():
+            model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=args['num_classes'], ignore_mismatched_sizes=True)
+        else:
+            model = AutoModelForSequenceClassification.from_pretrained(model_name)
+    elif args['type']=='regression':
+        model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=1, ignore_mismatched_sizes=True)
+    
+    return model.to(device)
+
+
+def validate(model, tokenizer,val_dataloader, metrics, args, device='cuda'):
+    model.eval()
+    with torch.no_grad():
+        
+        results=[]
+        for dataloader in val_dataloader:
+
+            predictions=[]
+            targets=[]
+
+            for val_batch in dataloader:
+
+                val_inputs=tokenizer(*[val_batch[k] for k in val_batch.keys() if (k!='label')and(k!='idx')], truncation=True, padding='max_length', max_length=128, return_tensors='pt').to(device)
+                outputs = model(input_ids=val_inputs['input_ids'], attention_mask=val_inputs['attention_mask'].to(torch.int32))[0]
+                
+                if args['type']=='classification':
+                    preds = outputs.argmax(-1)
+                elif args['type']=='regression':
+                    if args['final_activation']=='sigmoid':
+                        preds = torch.sigmoid(outputs[:,0])*(args['max']-args['min'])+args['min']
+                    else:
+                        preds = outputs[:,0]
+
+                predictions.append(preds)
+                targets.append(val_batch["label"])
+            
+            predictions=torch.concat(predictions,0).to(device)
+            targets=torch.concat(targets,0).to(device).to(torch.float32)
+
+            results+=[metric(predictions,targets).detach().cpu() for metric in metrics]
+    
+    model.train()
+    return results
+
+def Glue(model, tokenizer, task_name, args, epochs=10, lr=1e-5, batch_size=32, steps_validate=0.25, device='cuda'):
+
+    train_dataset = datasets.load_dataset("glue", task_name, split="train")
+    if 'val_datasets' in args.keys():
+        val_dataset = [datasets.load_dataset("glue", val_name, split="validation") for val_name in args['val_datasets']]
+    else:
+        val_dataset = [datasets.load_dataset("glue", task_name, split="validation")]
+
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_dataloader = [DataLoader(v_d, batch_size=batch_size, shuffle=False) for v_d in val_dataset]
+
+    if 'num_classes' not in args.keys():
+        args['num_classes']=2
+    metrics = []
+    for metric in args['metrics']:
+        if metric=='Accuracy':
+            metrics.append(torchmetrics.classification.MulticlassAccuracy(args['num_classes']).to(device))
+        elif metric=='F1':
+            metrics.append(torchmetrics.classification.MulticlassF1Score(args['num_classes']).to(device))
+        elif metric=='Matthews':
+            metrics.append(torchmetrics.classification.MulticlassMatthewsCorrCoef(args['num_classes']).to(device))
+        elif metric=='Pearson':
+            metrics.append(torchmetrics.PearsonCorrCoef().to(device))
+        elif metric=='Spearman':
+            metrics.append(torchmetrics.SpearmanCorrCoef().to(device))
+
+    if args['type']=='regression':
+        if 'min' not in args.keys():
+            args['min']=0
+        if 'min' not in args.keys():
+            args['max']=1
+
+    model.train()
+    optimizer = torch.optim.Adam(model.parameters(),lr=lr)
+
+    history=[]
+    val_history=[]
+
+    for epoch in range(epochs):
+        tot_loss=0
+        tot_elems=0
+        progress = tqdm(enumerate(train_dataloader), total=len(train_dataloader), desc=f'Epoch: {epoch+1}')
+        for batch_idx, batch in progress:
+
+            optimizer.zero_grad()
+
+            train_inputs = tokenizer(*[batch[k] for k in batch.keys() if (k!='label')and(k!='idx')], truncation=True, padding='max_length', max_length=128, return_tensors='pt').to(device)
+            outputs = model(input_ids=train_inputs['input_ids'], attention_mask=train_inputs['attention_mask'].to(torch.int32))
+            if args['type']=='classification':
+                loss = torch.nn.functional.cross_entropy(outputs[0],batch['label'].to(device))
+            elif args['type']=='regression':
+                if args['final_activation']=='sigmoid':
+                    preds=torch.sigmoid(outputs[0][:,0])*(args['max']-args['min'])+args['min']
+                else:
+                    preds = outputs[0][:,0]
+                loss = torch.nn.functional.mse_loss(preds,batch['label'].to(device).to(torch.float32))
+            else:
+                raise Exception("unknown task type")
+
+            loss.backward()
+            optimizer.step()
+
+            if steps_validate is not None:
+                if isinstance(steps_validate, float):
+                    steps_validate=int(steps_validate*len(train_dataloader))
+                if batch_idx % steps_validate == steps_validate-1:
+                    val_metrics = validate(model, tokenizer, val_dataloader, metrics, args)
+                    val_history.append((len(history),val_metrics))
+
+            tot_loss+=loss.detach().cpu()
+            tot_elems+=len(batch)
+            history.append(loss.detach().cpu())
+            progress.set_postfix_str(f"loss: {tot_loss/tot_elems:.3g}")
+
+    if 'val_datasets' in args.keys():
+        return  {'batch_per_epoch':len(train_dataloader),'history':history} , {'metrics': [ v_d + " " + m for m in args['metrics'] for v_d in args['val_datasets']] , 'val_history':val_history}
+    else:
+        return {'batch_per_epoch':len(train_dataloader),'history':history} , {'metrics': args['metrics'], 'val_history':val_history}
+
+
+def plot(H, V=None):
+    fig, ax1 = plt.subplots()
+    color = 'b'
+    ax1.set_xlabel('epochs')
+    ax1.set_ylabel('loss')
+    h=H['history']
+    n=len(h)//10
+    ax1.plot(np.arange(len(h))/H['batch_per_epoch'],h, color=color, alpha=0.2)
+    h1= np.convolve(h, np.ones(n)/n, mode='same')/(np.convolve(np.ones(len(h)), np.ones(n)/n, mode='same')+1e-7)
+    ax1.plot((np.arange(len(h1))+len(h)-len(h1))/H['batch_per_epoch'],h1, color=color)
+    ax1.xaxis.set_major_locator(MaxNLocator(integer=True))
+
+    if V is not None:
+        VH=V['val_history']
+        ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+        ax2.set_ylabel('metrics')
+        colors = plt.cm.autumn(np.linspace(0, 1, len(V['metrics'])+1))
+        for i, name in enumerate(V['metrics']):
+            ax2.plot([x/H['batch_per_epoch'] for x,y in VH],[y[i] for x,y in VH],".", color=colors[i])
+
+        ax2.legend([*V['metrics']])
+
+    #fig.tight_layout()  # otherwise the right y-label is slightly clipped
+    plt.show()
+
+
+def plot_results(run_dict):
+    styles=['--',':']
+
+    for task in run_dict.keys():
+        fig, ax = plt.subplots(figsize=(15,5))
+        fig.suptitle(task, fontsize=16)
+        runs = run_dict[task]
+        colors = plt.cm.plasma(np.linspace(0, 1, len(runs.keys())+1))
+        for i,run in enumerate(runs.keys()):
+            plt.subplot(1,2,1)
+            plt.title('training loss')
+            H=runs[run]['history']
+            h=H['history']
+            n=len(h)//10
+            plt.plot(np.arange(len(h))/H['batch_per_epoch'],h, color=colors[i], alpha=0.3)
+            h1= np.convolve(h, np.ones(n)/n, mode='same')/(np.convolve(np.ones(len(h)), np.ones(n)/n, mode='same')+1e-7)
+            plt.plot((np.arange(len(h1))+len(h)-len(h1))/H['batch_per_epoch'],h1, color=colors[i], label=run)
+            ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+            plt.legend()
+            
+            plt.subplot(1,2,2)
+            V=runs[run]['validation']
+            if V is not None:
+                VH=V['val_history']
+                plt.title('metrics')
+                for j, name in enumerate(V['metrics']):
+                    if (i==0):
+                        plt.plot([x/H['batch_per_epoch'] for x,y in VH],[y[j] for x,y in VH],styles[j], color=colors[i], label=name, alpha=0.7)
+                    else:
+                        plt.plot([x/H['batch_per_epoch'] for x,y in VH],[y[j] for x,y in VH],styles[j], color=colors[i], alpha=0.7)
+            plt.legend([*V['metrics']])
